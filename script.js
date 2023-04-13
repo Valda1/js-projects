18b79526b2feed0c664d8cee94c1106bef757bd7
Valda <valda.bimbirule1@gmail.com>
2023-04-13 16:46:07 +0300

    Updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let addButton = document.getElementById('add');
-let toDoContainer = document.getElementById('to-do-container');
-let inputField = document.getElementById('task');
+//let addButton = document.getElementById('add');
+let listContainer = document.getElementById('list-container');
+let inputField = document.getElementById('input-box');
 
-addButton.addEventListener('click', function(){
+/*addButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
@@ -14,4 +14,39 @@ addButton.addEventListener('click', function(){
     paragraph.addEventListener('dblclick', function(){
         paragraph.removeChild(paragraph);
     });
-});
+});*/
+
+const addTask = () => {
+    if(inputField.value === ''){
+        alert('You must write something!')
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputField.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputField.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHtml);
+};
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+};
+
+showTask();
