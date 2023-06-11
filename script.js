8ad46d9ec146b788d43d6365bbd3d3f910eb541a
Valda <valda.bimbirule1@gmail.com>
2023-06-11 22:18:08 +0300

    Updates

--- script.js
+++ script.js
@@ -1,37 +1,90 @@
-//let addButton = document.getElementById('add');
-let listContainer = document.getElementById('list-container');
-let inputField = document.getElementById('input-box');
+const listContainer = document.getElementById("list-container");
+const inputField = document.getElementById("input-box");
+const addButton = document.getElementById("add-button");
 
-/*addButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    });
-    paragraph.addEventListener('dblclick', function(){
-        paragraph.removeChild(paragraph);
-    });
-});*/
+addButton.addEventListener("click", function(){
+        let span = document.createElement("span");
+        let li = document.createElement("li");
+        li.innerHTML = inputField.value;
+        let checkButton = document.createElement("button");
+        //checkButton.innerHTML = '<i class="fa fa-check"></i>';
+        checkButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        let editButton = document.createElement("button");
+        editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+        let deleteButton = document.createElement("button");
+        //deleteButton.innerHTML = '<i class = "fa fa-trash-o"></i>';
+        deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
 
-const addTask = () => {
+        span.appendChild(li);
+        span.appendChild(checkButton);
+        span.appendChild(editButton);
+        span.appendChild(deleteButton);
+
+        listContainer.appendChild(span);
+
+        deleteButton.addEventListener("click", function(){
+            const parent = deleteButton.parentElement;
+            parent.parentElement.removeChild(parent);
+        });
+
+        checkButton.addEventListener("click", function(){
+            const item = checkButton.parentElement;
+            const li = item.querySelector("li");
+            li.style.textDecoration = "line-through";
+        });
+
+        editButton.addEventListener("click", function(){
+            inputField.value = li.innerText;
+            const parent = editButton.parentElement;
+            parent.parentElement.removeChild(parent);
+        });
+});
+
+/*const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML);
+};*/
+
+/*const addTask = () => {
     if(inputField.value === ''){
         alert('You must write something!')
     }else{
+        let span = document.createElement("span");
         let li = document.createElement("li");
         li.innerHTML = inputField.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+        let checkButton = document.createElement("button");
+        checkButton.innerHTML = '<i class="fa fa-check"></i>';
+        let deleteButton = document.createElement("button");
+        deleteButton.innerHTML = '<i class = "fa fa-trash-o"></i>';
+
+        span.appendChild(li);
+        span.appendChild(checkButton);
+        span.appendChild(deleteButton);
+
+        listContainer.appendChild(span);
     }
     inputField.value = "";
     saveData();
-}
+}*/
 
-listContainer.addEventListener("click", function(e){
+/*const deleteTask = (task) => {
+    if(deleteButton){
+        deleteButton.addEventListener("click", function(){
+            span.parentNode.removeChild(span);
+        })
+    }
+    listContainer.removeChild(task);
+
+}*/
+
+/*deleteButton.addEventListener("click", function(){
+    /*const parent = this.parentNode;
+    parent.remove();
+    saveData();
+    span.parentNode.removeChild(span);
+    alert("click");
+})*/
+
+/*listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
         saveData();
@@ -39,14 +92,10 @@ listContainer.addEventListener("click", function(e){
         e.target.parentElement.remove();
         saveData();
     }
-}, false);
+}, false);*/
 
-const saveData = () => {
-    localStorage.setItem("data", listContainer.innerHtml);
-};
-
-const showTask = () => {
+/*const showTask = () => {
     listContainer.innerHTML = localStorage.getItem("data");
-};
+};*/
 
-showTask();
+//showTask();
