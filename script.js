570d0e6c236454a2bbd6f3ec2ec337a166a7e596
Valda <valda.bimbirule1@gmail.com>
2023-06-12 22:14:36 +0300

    Some updates

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const listContainer = document.getElementById("list-container");
 const inputField = document.getElementById("input-box");
 const addButton = document.getElementById("add-button");
 
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML);
+    //listContainer.innerHTML = localStorage.getItem("data");
+};
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+};
+
 addButton.addEventListener("click", function(){
         let span = document.createElement("span");
         let li = document.createElement("li");
@@ -22,6 +31,10 @@ addButton.addEventListener("click", function(){
 
         listContainer.appendChild(span);
 
+        inputField.value = "";
+
+        saveData();
+
         deleteButton.addEventListener("click", function(){
             const parent = deleteButton.parentElement;
             parent.parentElement.removeChild(parent);
@@ -39,63 +52,5 @@ addButton.addEventListener("click", function(){
             parent.parentElement.removeChild(parent);
         });
 });
-
-/*const saveData = () => {
-    localStorage.setItem("data", listContainer.innerHTML);
-};*/
-
-/*const addTask = () => {
-    if(inputField.value === ''){
-        alert('You must write something!')
-    }else{
-        let span = document.createElement("span");
-        let li = document.createElement("li");
-        li.innerHTML = inputField.value;
-        let checkButton = document.createElement("button");
-        checkButton.innerHTML = '<i class="fa fa-check"></i>';
-        let deleteButton = document.createElement("button");
-        deleteButton.innerHTML = '<i class = "fa fa-trash-o"></i>';
-
-        span.appendChild(li);
-        span.appendChild(checkButton);
-        span.appendChild(deleteButton);
-
-        listContainer.appendChild(span);
-    }
-    inputField.value = "";
-    saveData();
-}*/
-
-/*const deleteTask = (task) => {
-    if(deleteButton){
-        deleteButton.addEventListener("click", function(){
-            span.parentNode.removeChild(span);
-        })
-    }
-    listContainer.removeChild(task);
-
-}*/
-
-/*deleteButton.addEventListener("click", function(){
-    /*const parent = this.parentNode;
-    parent.remove();
-    saveData();
-    span.parentNode.removeChild(span);
-    alert("click");
-})*/
-
-/*listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);*/
-
-/*const showTask = () => {
-    listContainer.innerHTML = localStorage.getItem("data");
-};*/
 
 //showTask();
