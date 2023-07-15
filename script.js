0459df0c88bcc5e7229d9f0019480a3136d2342a
Valda <valda.bimbirule1@gmail.com>
2023-07-15 13:24:27 +0300

    New code

--- script.js
+++ script.js
@@ -1,56 +1,75 @@
-const listContainer = document.getElementById("list-container");
-const inputField = document.getElementById("input-box");
-const addButton = document.getElementById("add-button");
+const todoList = document.getElementById('todo-list');
+let savedItems = JSON.parse(localStorage.getItem('todoItems')) || [];
 
-const saveData = () => {
-    localStorage.setItem("data", listContainer.innerHTML);
-    //listContainer.innerHTML = localStorage.getItem("data");
-};
+// Render the saved items on the screen
+function renderTodoList() {
+  // Clear the todoList element
+  todoList.innerHTML = '';
+  
+  // Render each item in the savedItems array
+  savedItems.forEach((item, index) => {
+    // Create a new list item for the item
+    const li = document.createElement('li');
+    li.innerText = item;
+    
+    // Create buttons for crossing out, editing, and deleting the item
+    const crossOutButton = document.createElement('button');
+    crossOutButton.innerText = '✓';
+    crossOutButton.addEventListener('click', () => {
+      li.classList.toggle('crossed-out');
+      updateSavedItems();
+    });
+    
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => {
+      const newTitle = prompt('Enter the new title for this item:', item);
+      if (newTitle) {
+        savedItems[index] = newTitle;
+        updateSavedItems();
+        renderTodoList();
+      }
+    });
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'X';
+    deleteButton.addEventListener('click', () => {
+      savedItems.splice(index, 1);
+      updateSavedItems();
+      renderTodoList();
+    });
+    
+    // Add the buttons to the list item, and add the list item to the todoList element
+    li.appendChild(crossOutButton);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+  });
+}
 
-const showTask = () => {
-    listContainer.innerHTML = localStorage.getItem("data");
-};
-
-addButton.addEventListener("click", function(){
-        let span = document.createElement("span");
-        let li = document.createElement("li");
-        li.innerHTML = inputField.value;
-        let checkButton = document.createElement("button");
-        //checkButton.innerHTML = '<i class="fa fa-check"></i>';
-        checkButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-        let editButton = document.createElement("button");
-        editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
-        let deleteButton = document.createElement("button");
-        //deleteButton.innerHTML = '<i class = "fa fa-trash-o"></i>';
-        deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-
-        span.appendChild(li);
-        span.appendChild(checkButton);
-        span.appendChild(editButton);
-        span.appendChild(deleteButton);
-
-        listContainer.appendChild(span);
-
-        inputField.value = "";
-
-        saveData();
-
-        deleteButton.addEventListener("click", function(){
-            const parent = deleteButton.parentElement;
-            parent.parentElement.removeChild(parent);
-        });
-
-        checkButton.addEventListener("click", function(){
-            const item = checkButton.parentElement;
-            const li = item.querySelector("li");
-            li.style.textDecoration = "line-through";
-        });
-
-        editButton.addEventListener("click", function(){
-            inputField.value = li.innerText;
-            const parent = editButton.parentElement;
-            parent.parentElement.removeChild(parent);
-        });
+// Add an event listener to the form to save new items to local storage
+const form = document.getElementById('todo-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.getElementById('todo-input');
+  const newItem = input.value;
+  savedItems.push(newItem);
+  updateSavedItems();
+  renderTodoList();
+  input.value = '';
 });
 
-//showTask();
+// Update the saved items in local storage
+function updateSavedItems() {
+  localStorage.setItem('todoItems', JSON.stringify(savedItems));
+}
+
+// Render the initial todo list when the page loads
+renderTodoList();
+
+/*let crossOutButton = document.createElement("button");
+crossOutButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+let editButton = document.createElement("button");
+editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+let deleteButton = document.createElement("button");
+deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';*/
