00721e32858ec4b0513ec5bb5a5ec62985f4f665
Valda <valda.bimbirule1@gmail.com>
2023-10-23 08:40:31 +0300

    Final commit

--- react-chatgpt/server.js
+++ react-chatgpt/server.js
@@ -1,11 +1,13 @@
 const PORT = 8000;
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const API_KEY = 'API_KEY';
+const API_KEY = process.env.API_KEY;
+//const API_KEY = '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';
 
 app.post('/completions', async (req, res) => {
     const options = {
